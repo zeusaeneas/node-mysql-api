965cc50ac8f12ec4ebff0341c2a2197154c851bc
zeusaeneas <jeusaeneas@gmail.com>
2026-05-18 16:20:02 -0400

    add TiDB cloud support with env variables

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -10,14 +10,23 @@ export default db;
 initialize();
 
 async function initialize() {
-    const { host, port, user, password, database } = config.database;
+    const host = process.env.DB_HOST || config.database.host;
+    const port = parseInt(process.env.DB_PORT || '') || config.database.port;
+    const user = process.env.DB_USER || config.database.user;
+    const password = process.env.DB_PASSWORD || config.database.password;
+    const database = process.env.DB_NAME || config.database.database;
 
     // Create DB if it doesn't exist
-    const connection = await mysql.createConnection({ host, port, user, password });
+    const connection = await mysql.createConnection({ host, port, user, password, ssl: { rejectUnauthorized: false } });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // Connect to DB
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, {
+        host,
+        port,
+        dialect: 'mysql',
+        dialectOptions: { ssl: { rejectUnauthorized: false } }
+    });
 
     // Init models
     db.Account = accountModel(sequelize);
