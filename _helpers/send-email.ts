12161463ee02ed671ffd89d2e5fdd0fd88524e08
zeusaeneas <jeusaeneas@gmail.com>
2026-05-20 13:29:40 -0400

    updates

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -2,6 +2,13 @@ import nodemailer from 'nodemailer';
 import config from '../config.json';
 
 export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
-    const transporter = nodemailer.createTransport(config.smtpOptions);
-    await transporter.sendMail({ from, to, subject, html});
+    try {
+        const transporter = nodemailer.createTransport(config.smtpOptions as any);
+        await transporter.verify(); // ← add this to test connection
+        const info = await transporter.sendMail({ from, to, subject, html });
+        console.log('Email sent:', info.messageId);
+    } catch (error) {
+        console.error('Email error:', error); // ← this will show in Render logs
+        throw error;
+    }
 }
